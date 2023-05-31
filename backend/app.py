729691236711b55fad80cc4f2d09b7cad8612ddf
therealnightmare <nnahid929@gmail.com>
2023-06-01 03:31:28 +0600

    first commit

---
+++ backend/app.py
@@ -0,0 +1,86 @@
+
+from flask import Flask, jsonify, request 
+from flask_cors import CORS
+import uuid
+
+app = Flask(__name__)
+
+app.config.from_object(__name__)
+
+CORS(app, resources={r"/*":{'origins':"*"}})
+
+GAMES = [
+
+    {   'id': uuid.uuid4().hex,
+        'title':'2k21',
+        'genre':'sports',
+        'played': True,
+    },
+    {   'id': uuid.uuid4().hex,
+        'title':'Evil Within',
+        'genre':'horror',
+        'played': False,
+    },
+    {   'id': uuid.uuid4().hex,
+        'title':'the last of us',
+        'genre':'survival',
+        'played': True,
+    },
+    {  'id': uuid.uuid4().hex,
+        'title':'days gone',
+        'genre':'horror/survival',
+        'played': False,
+    },
+    {   'id': uuid.uuid4().hex,
+        'title':'mario',
+        'genre':'retro',
+        'played': True,
+    }
+
+]
+
+@app.route('/', methods=['GET', 'POST'])
+def all_games():
+    response_object = {'status':'success'}
+    if request.method == "POST":
+        post_data = request.get_json()
+        GAMES.append({
+            'id' : uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'genre': post_data.get('genre'),
+            'played': post_data.get('played')})
+        response_object['message'] =  'Game Added!'
+    else:
+        response_object['games'] = GAMES
+    return jsonify(response_object)
+
+
+@app.route('/<game_id>', methods =['PUT', 'DELETE'])
+def single_game(game_id):
+    response_object = {'status':'success'}
+    if request.method == "PUT":
+        post_data = request.get_json()
+        remove_game(game_id)
+        GAMES.append({
+            'id' : uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'genre': post_data.get('genre'),
+            'played': post_data.get('played')
+        })
+        response_object['message'] =  'Game Updated!'
+    if request.method == "DELETE":
+        remove_game(game_id)
+        response_object['message'] = 'Game removed!'    
+    return jsonify(response_object)
+
+
+def remove_game(game_id):
+    for game in GAMES:
+        if game['id'] == game_id:
+            GAMES.remove(game)
+            return True
+    return False
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
